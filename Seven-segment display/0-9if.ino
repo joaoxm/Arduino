1d76e4ca982aad0b52032f1105e2dab98a0bfde3
joaoxm <jgsantista@gmail.com>
2014-04-29 19:46:59 -0300

    Fix

--- Seven-segment display/0-9if.ino
+++ Seven-segment display/0-9if.ino
@@ -1,36 +1,36 @@
 int count = 0;
 int test = 0;
-int ledPin[] = {4, 5, 6, 7};
+int ledPin[] = {4, 5, 6};
 const int REFRESH_TIME = 1000;
 
 void setup(){
-	for (i=0; i<=3; i++){pinMode(ledPin[i], OUTPUT)}
+	for (i=4; i<=6; i++){pinMode(ledPin[i], OUTPUT)}
 }
 
 void loop(){
 	test = count;
 	if (test-8>=0){
-		digitalWrite(pin[0], HIGH);
+		digitalWrite(ledPin[0], HIGH);
 		test -= 8;
 	} else {
-		digitalWrite(pin[0], LOW);
+		digitalWrite(ledPin[0], LOW);
 	}
 	if (test-4>=0){
-		digitalWrite(pin[1], HIGH);
+		digitalWrite(ledPin[1], HIGH);
 		test -= 4;
 	} else {
-		digitalWrite(pin[1], LOW);
+		digitalWrite(ledPin[1], LOW);
 	}
 	if (test-2>=0){
-		digitalWrite(pin[2], HIGH);
+		digitalWrite(ledPin[2], HIGH);
 		test -= 2;
 	} else {
-		digitalWrite(pin[2], LOW);
+		digitalWrite(ledPin[2], LOW);
 	}
 	if (test-1>=0){
-		digitalWrite(pin[3], HIGH);
+		digitalWrite(ledPin[3], HIGH);
 	} else {
-		digitalWrite(pin[3], LOW);
+		digitalWrite(ledPin[3], LOW);
 	}
 	delay(REFRESH_TIME);
 	count += 1;

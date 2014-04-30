a731aa15ef09b6e5c20fb8bcf2eee4a822338f1f
joaoxm <jgsantista@gmail.com>
2014-04-30 14:06:40 -0300

    Seven-segment display 0-99
    From 0 to 99 in seven-segment display

--- Seven-segment display/0-99if.ino
+++ Seven-segment display/0-99if.ino
@@ -1,14 +1,18 @@
-float count, test, help = 0;
+float count, test, help, valor = 0;
 int a, b, help = 0;
 int ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11}; //4,5,6,7 display esquerda, 8,9,10,11 display direita
 const int REFRESH_TIME = 100;
 
 void setup(){
 	for (i=4; i<=11; i++){pinMode(ledPin[i], OUTPUT)}
+	Serial.begin(9600);
 }
 
 void loop(){
 	count = 1.3; //RECEBE VOLTAGEM
+	//count = analogRead(A1);
+	Serial.print("Voltagem: ");
+	Serial.println(count);
 	test = int(count); //recebeu 1
 	
 	if (test-8>=0){
@@ -45,6 +49,7 @@ void loop(){
 		
 		if (a != help){ //2 != 1
 			test = 10 - b;
+			break;
 		}	
 	}
 	
@@ -71,6 +76,20 @@ void loop(){
 	} else {
 		digitalWrite(ledPin[7], LOW);
 	}
+	
+	if (count < 0,5){ 
+    digitalWrite(ledPin[0], LOW);
+    digitalWrite(ledPin[1], LOW);
+    digitalWrite(ledPin[2], HIGH);
+  } else if ((count > 0,5) AND (count < 1,0)){
+    digitalWrite(ledPin[0], HIGH);
+    digitalWrite(ledPin[1], LOW);
+    digitalWrite(ledPin[2], LOW);    
+  } else {
+   digitalWrite(ledPin[0], LOW);
+   digitalWrite(ledPin[1], HIGH);
+   digitalWrite(ledPin[2], LOW);     
+  }
 	
 	delay(REFRESH_TIME);
 	count += 0.1;
